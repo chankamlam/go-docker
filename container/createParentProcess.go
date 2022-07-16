41c7496ebabaa36f5a1def15ec9dec5e7179239e
Ken <chankamlam@icloud.com>
2022-07-16 22:06:52 +0800

    add

--- container/createParentProcess.go
+++ container/createParentProcess.go
@@ -6,7 +6,7 @@ import(
 )
 
 func CreateParentProcess(interactive bool,tty bool,args []string) *exec.Cmd {
-	cmd:=exec.Command("/proc/self/exe","child",args[0])
+	cmd:=exec.Command("/proc/self/exe","child",args)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags:syscall.CLONE_NEWUTS|syscall.CLONE_NEWPID|syscall.CLONE_NEWNS|syscall.CLONE_NEWIPC|syscall.CLONE_NEWNET|syscall.CLONE_NEWUSER,
 		UidMappings: []syscall.SysProcIDMap{
